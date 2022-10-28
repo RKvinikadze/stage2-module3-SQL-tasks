d4b7085da95d1779a070df3f63f720930a19022b
romikokvinikadze <r.kvinikadze11@gmail.com>
2022-10-28 10:13:44 +0400

    part2

---
+++ lesson12/src/main/resources/SOLUTION.sql
@@ -0,0 +1,21 @@
+delete
+from STUDENT
+where (
+select STUDENT_ID from SUBJECT
+inner join MARK M on SUBJECT.ID = M.SUBJECT_ID
+inner join STUDENT on M.STUDENT_ID = STUDENT.ID
+where MARK > 4
+);
+
+delete from STUDENT
+where ID = (
+    select STUDENT_ID from MARK
+    group by STUDENT_ID
+    having min(MARK) < 4
+);
+
+delete from PAYMENTTYPE
+where NAME = 'Daily';
+
+delete from MARK where MARK < 7;
+
--- lesson12/src/main/resources/TABLES_CHANGES.sql
+++ lesson12/src/main/resources/TABLES_CHANGES.sql
@@ -1,1 +1,23 @@
 select * from student;
+
+alter table MARK
+drop constraint if exists mark_student_id_fkey;
+
+alter table MARK
+add constraint mark_student_id_fkey foreign key (student_id) references student(id)
+on delete cascade;
+
+
+alter table PAYMENT
+drop constraint if exists payment_student_id_fkey;
+
+alter table PAYMENT
+add constraint payment_student_id_fkey foreign key (student_id) references student(id)
+on delete cascade;
+
+alter table PAYMENT
+drop constraint if exists payment_type_id_fkey;
+
+alter table PAYMENT
+add constraint payment_type_id_fkey foreign key (type_id) references paymenttype(id)
+on delete cascade;
